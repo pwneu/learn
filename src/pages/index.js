0411773450d4c34a6bf652238be17ab7774bcb3c
Sharon Grace T. Hangaan <127215110+sghangaan@users.noreply.github.com>
2024-09-14 16:20:09 +0800

    Modify title

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,8 +33,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`Welcome to Our Platform`}
+      description="Explore our features, participate in challenges, and enjoy your experience on our platform. <head />"
     >
       <HomepageHeader />
       <main>
